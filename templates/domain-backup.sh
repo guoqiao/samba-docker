caea5dcffad10e91130e3dfa478a2edcda02c098
Guo Qiao <guoqiao@gmail.com>
2019-03-28 16:39:47 +1300

    add SAMBA_VOLUME
    Signed-off-by: Guo Qiao <guoqiao@gmail.com>

--- templates/domain-backup.sh
+++ templates/domain-backup.sh
@@ -4,4 +4,4 @@ set -xue
 # backup, export and rename
 export TARGETDIR=/tmp/sambabackup
 sudo python3 /usr/local/samba/bin/samba-tool domain backup offline --targetdir=${TARGETDIR}
-sudo mv ${TARGETDIR}/*.tar.bz2 /volume/samba-backup-docker-{{SAMBA_BACKEND_STORE}}-${NUM_USERS}-max-${NUM_MAX_MEMBERS}.tar.bz2
+sudo mv ${TARGETDIR}/*.tar.bz2 {{SAMBA_VOLUME}}/samba-backup-docker-{{SAMBA_BACKEND_STORE}}-${NUM_USERS}-max-${NUM_MAX_MEMBERS}.tar.bz2
