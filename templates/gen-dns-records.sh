134f1948adda56610a6ce16f763fbc6292685523
Guo Qiao <guoqiao@gmail.com>
2019-03-28 15:51:55 +1300

    templates/gen-dns-records.sh: update
    Signed-off-by: Guo Qiao <guoqiao@gmail.com>

--- templates/gen-dns-records.sh
+++ templates/gen-dns-records.sh
@@ -5,21 +5,16 @@ SAMBA_TOOL="sudo python3 /usr/local/samba/bin/samba-tool"
 SAMBA_REALM=samdom.example.com
 SAMBA_CRED="-U Administrator%Password01@"
 
-
 # generate zone0, zone1...
 for i in {0..19}
 do
-    $SAMBA_TOOL \
-        dns zonecreate localhost zone$i.$SAMBA_REALM \
-        $SAMBA_CRED || true
+    $SAMBA_TOOL dns zonecreate localhost zone$i.$SAMBA_REALM $SAMBA_CRED
 done
 
 # add NS record for dc0, dc1..., dc59 to zone0
 for i in {0..59}
 do
-    $SAMBA_TOOL \
-        dns add localhost zone0.$SAMBA_REALM dc$i NS dc$i.$SAMBA_REALM \
-        $SAMBA_CRED
+    $SAMBA_TOOL dns add localhost zone0.$SAMBA_REALM dc$i NS dc$i.$SAMBA_REALM $SAMBA_CRED
 done
 
 # generate NS record for each user in zone1
