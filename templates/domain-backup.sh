4b7dbfa8413d97840e80e56cafa59c2b23d392b6
Guo Qiao <guoqiao@gmail.com>
2019-04-01 11:18:58 +1300

    templates/domain-backup.sh: add default values for NUM_USERS and NUM_MAX_MEMBERS
    Signed-off-by: Guo Qiao <guoqiao@gmail.com>

--- templates/domain-backup.sh
+++ templates/domain-backup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -xue
 
+NUM_USERS=${1-1000}
+NUM_MAX_MEMBERS=${2-1000}
+
 # backup, export and rename
 export TARGETDIR=/tmp/sambabackup
 sudo python3 /usr/local/samba/bin/samba-tool domain backup offline --targetdir=${TARGETDIR}
