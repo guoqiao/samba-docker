37fffcebf279c9c426285374a892dfda094dd996
Guo Qiao <guoqiao@gmail.com>
2019-04-02 09:49:49 +1300

    templates/gen-dns-records.sh: refactor
    Signed-off-by: Guo Qiao <guoqiao@gmail.com>

--- templates/gen-dns-records.sh
+++ templates/gen-dns-records.sh
@@ -8,46 +8,38 @@ SAMBA_CRED="--username {{ SAMBA_USERNAME }} --password {{ SAMBA_PASSWORD }}"
 SAMBA_ZONE_DEFAULT={{SAMBA_REALM}}
 SAMBA_ZONE_MSDCS=_msdcs.{{SAMBA_REALM}}
 
-IPV4_ADDR=10.10.10.255
-IPV6_ADDR=2404:130:0:1000:d8c6:a0eb:ef0a:d6a7
+IPV4_ADDR=10.10.10.10
 
-# create extra zones zone[1-20]
-for i in {1..20}
+# create zones
+for zone in user computer srv
 do
-    $SAMBA_TOOL dns zonecreate localhost zone$i.$SAMBA_REALM $SAMBA_CRED
+    $SAMBA_TOOL dns zonecreate localhost $SAMBA_CRED $zone.$SAMBA_REALM
 done
+$SAMBA_TOOL dns zonecreate localhost $SAMBA_CRED 10.10.10.in-addr.arpa
 
 # samba-tool dns add <server> <zone> <name> <A|AAAA|PTR|CNAME|NS|MX|SRV|TXT> <data>
 for i in {1..60}
 do
-    # add A/AAAA record for each DC to default zone
-    $SAMBA_TOOL dns add localhost $SAMBA_ZONE_DEFAULT dc$i.$SAMBA_REALM  A    $IPV4_ADDR $SAMBA_CRED
-    $SAMBA_TOOL dns add localhost $SAMBA_ZONE_DEFAULT dc$i.$SAMBA_REALM  AAAA $IPV6_ADDR $SAMBA_CRED
-
-    # add A/AAAA record for each DC to msdcs zone
-    $SAMBA_TOOL dns add localhost $SAMBA_ZONE_MSDCS   dc$i.$SAMBA_REALM  A    $IPV4_ADDR $SAMBA_CRED
-    $SAMBA_TOOL dns add localhost $SAMBA_ZONE_MSDCS   dc$i.$SAMBA_REALM  AAAA $IPV6_ADDR $SAMBA_CRED
-
-    # set each DC as nameserver for both zones
-    $SAMBA_TOOL dns add localhost $SAMBA_ZONE_DEFAULT $SAMBA_REALM NS dc$i.$SAMBA_REALM $SAMBA_CRED
-    $SAMBA_TOOL dns add localhost $SAMBA_ZONE_MSDCS   $SAMBA_REALM NS dc$i.$SAMBA_REALM $SAMBA_CRED
-
+    $SAMBA_TOOL dns add localhost $SAMBA_CRED $SAMBA_ZONE_MSDCS   dc$i.$SAMBA_REALM  A  $IPV4_ADDR
+    $SAMBA_TOOL dns add localhost $SAMBA_CRED $SAMBA_ZONE_DEFAULT $SAMBA_REALM NS dc$i.$SAMBA_REALM
 done
 
+echo add A/PTR record for each user into user zone
 for name in $($SAMBA_TOOL user list)
 do
-    # add A/AAAA record for @/* for each user
-    $SAMBA_TOOL dns add localhost $SAMBA_ZONE_DEFAULT   $name.$SAMBA_REALM  A    $IPV4_ADDR $SAMBA_CRED
-    $SAMBA_TOOL dns add localhost $SAMBA_ZONE_DEFAULT *.$name.$SAMBA_REALM  A    $IPV4_ADDR $SAMBA_CRED
-    $SAMBA_TOOL dns add localhost $SAMBA_ZONE_DEFAULT   $name.$SAMBA_REALM  AAAA $IPV6_ADDR $SAMBA_CRED
-    $SAMBA_TOOL dns add localhost $SAMBA_ZONE_DEFAULT *.$name.$SAMBA_REALM  AAAA $IPV6_ADDR $SAMBA_CRED
+    $SAMBA_TOOL dns add localhost $SAMBA_CRED user.$SAMBA_REALM   $name.$SAMBA_REALM A     $IPV4_ADDR
+    $SAMBA_TOOL dns add localhost $SAMBA_CRED user.$SAMBA_REALM *.$name.$SAMBA_REALM A     $IPV4_ADDR
+    $SAMBA_TOOL dns add localhost $SAMBA_CRED 10.10.10.in-addr.arpa 10 PTR   $name.$SAMBA_REALM
+    $SAMBA_TOOL dns add localhost $SAMBA_CRED 10.10.10.in-addr.arpa 10 PTR *.$name.$SAMBA_REALM
 done
 
+echo add A/PTR record for each computer into computer zone
 for name in $($SAMBA_TOOL computer list)
 do
-    # add A/AAAA record for @/* for each machine
-    $SAMBA_TOOL dns add localhost $SAMBA_ZONE_DEFAULT   $name.$SAMBA_REALM A    $IPV4_ADDR $SAMBA_CRED
-    $SAMBA_TOOL dns add localhost $SAMBA_ZONE_DEFAULT *.$name.$SAMBA_REALM A    $IPV4_ADDR $SAMBA_CRED
-    $SAMBA_TOOL dns add localhost $SAMBA_ZONE_DEFAULT   $name.$SAMBA_REALM AAAA $IPV6_ADDR $SAMBA_CRED
-    $SAMBA_TOOL dns add localhost $SAMBA_ZONE_DEFAULT *.$name.$SAMBA_REALM AAAA $IPV6_ADDR $SAMBA_CRED
+    $SAMBA_TOOL dns add localhost $SAMBA_CRED computer.$SAMBA_REALM   $name.$SAMBA_REALM A     $IPV4_ADDR
+    $SAMBA_TOOL dns add localhost $SAMBA_CRED computer.$SAMBA_REALM *.$name.$SAMBA_REALM A     $IPV4_ADDR
+    $SAMBA_TOOL dns add localhost $SAMBA_CRED 10.10.10.in-addr.arpa 10 PTR   $name.$SAMBA_REALM
+    $SAMBA_TOOL dns add localhost $SAMBA_CRED 10.10.10.in-addr.arpa 10 PTR *.$name.$SAMBA_REALM
+    # add SRV record for each computer to srv zone
+    $SAMBA_TOOL dns add localhost $SAMBA_CRED srv.$SAMBA_REALM srv.$name SRV '$name.$SAMBA_REALM 8080 0 100'
 done
