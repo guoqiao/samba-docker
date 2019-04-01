9dc5348d2ddfc0ec559ebf20e68f5fc057a39ab4
Guo Qiao <guoqiao@gmail.com>
2019-04-02 11:23:14 +1300

    add samba_dns_backend and use it in domain-provision.sh
    Signed-off-by: Guo Qiao <guoqiao@gmail.com>

--- templates/domain-provision.sh
+++ templates/domain-provision.sh
@@ -5,6 +5,7 @@ sudo python3 /usr/local/samba/bin/samba-tool domain provision \
     --server-role=dc \
     --realm="{{SAMBA_REALM}}" \
     --domain="{{SAMBA_DOMAIN}}" \
+    --dns-backend="{{samba_dns_backend}}" \
     --backend-store='{{SAMBA_BACKEND_STORE}}' \
     --adminpass='{{SAMBA_PASSWORD}}' \
     --krbtgtpass='{{SAMBA_PASSWORD}}' \
