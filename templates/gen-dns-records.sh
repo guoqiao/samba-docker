b59e5f5c3532cb4467ff75b242d9325d81835310
Guo Qiao <guoqiao@gmail.com>
2019-04-04 18:29:12 +1300

    templates/gen-dns-records.sh: gen NS for zones and improve
    Signed-off-by: Guo Qiao <guoqiao@gmail.com>

--- templates/gen-dns-records.sh
+++ templates/gen-dns-records.sh
@@ -5,23 +5,29 @@ SAMBA_TOOL="sudo python3 /usr/local/samba/bin/samba-tool"
 SAMBA_REALM={{SAMBA_REALM}}
 SAMBA_CRED="--username {{ SAMBA_USERNAME }} --password {{ SAMBA_PASSWORD }}"
 # samba will create 2 default zones
-SAMBA_ZONE_DEFAULT={{SAMBA_REALM}}
-SAMBA_ZONE_MSDCS=_msdcs.{{SAMBA_REALM}}
+SAMBA_ZONE_DEFAULT=$SAMBA_REALM
+SAMBA_ZONE_PTR=10.10.10.in-addr.arpa
 
 IPV4_ADDR=10.10.10.10
+
 
 # create zones
 for zone in user computer srv
 do
     $SAMBA_TOOL dns zonecreate localhost $SAMBA_CRED $zone.$SAMBA_REALM
 done
-$SAMBA_TOOL dns zonecreate localhost $SAMBA_CRED 10.10.10.in-addr.arpa
+$SAMBA_TOOL dns zonecreate localhost $SAMBA_CRED $SAMBA_ZONE_PTR
 
-# samba-tool dns add <server> <zone> <name> <A|AAAA|PTR|CNAME|NS|MX|SRV|TXT> <data>
+echo "add all DCs as NS record for all zones"
+echo "bind/bind9/named required all zones to have NS records"
 for i in {1..60}
 do
-    $SAMBA_TOOL dns add localhost $SAMBA_CRED $SAMBA_ZONE_MSDCS   dc$i.$SAMBA_REALM  A  $IPV4_ADDR
-    $SAMBA_TOOL dns add localhost $SAMBA_CRED $SAMBA_ZONE_DEFAULT $SAMBA_REALM NS dc$i.$SAMBA_REALM
+    $SAMBA_TOOL dns add localhost $SAMBA_CRED          $SAMBA_REALM $SAMBA_REALM NS dc$i.$SAMBA_REALM
+    $SAMBA_TOOL dns add localhost $SAMBA_CRED   _msdcs.$SAMBA_REALM $SAMBA_REALM NS dc$i.$SAMBA_REALM
+    $SAMBA_TOOL dns add localhost $SAMBA_CRED     user.$SAMBA_REALM $SAMBA_REALM NS dc$i.$SAMBA_REALM
+    $SAMBA_TOOL dns add localhost $SAMBA_CRED computer.$SAMBA_REALM $SAMBA_REALM NS dc$i.$SAMBA_REALM
+    $SAMBA_TOOL dns add localhost $SAMBA_CRED      srv.$SAMBA_REALM $SAMBA_REALM NS dc$i.$SAMBA_REALM
+    $SAMBA_TOOL dns add localhost $SAMBA_CRED       $SAMBA_ZONE_PTR $SAMBA_REALM NS dc$i.$SAMBA_REALM
 done
 
 echo add A/PTR record for each user into user zone
@@ -29,8 +35,8 @@ for name in $($SAMBA_TOOL user list)
 do
     $SAMBA_TOOL dns add localhost $SAMBA_CRED user.$SAMBA_REALM   $name.$SAMBA_REALM A     $IPV4_ADDR
     $SAMBA_TOOL dns add localhost $SAMBA_CRED user.$SAMBA_REALM *.$name.$SAMBA_REALM A     $IPV4_ADDR
-    $SAMBA_TOOL dns add localhost $SAMBA_CRED 10.10.10.in-addr.arpa 10 PTR   $name.$SAMBA_REALM
-    $SAMBA_TOOL dns add localhost $SAMBA_CRED 10.10.10.in-addr.arpa 10 PTR *.$name.$SAMBA_REALM
+    $SAMBA_TOOL dns add localhost $SAMBA_CRED $SAMBA_ZONE_PTR 10 PTR   $name.$SAMBA_REALM
+    $SAMBA_TOOL dns add localhost $SAMBA_CRED $SAMBA_ZONE_PTR 10 PTR *.$name.$SAMBA_REALM
 done
 
 echo add A/PTR record for each computer into computer zone
@@ -38,8 +44,8 @@ for name in $($SAMBA_TOOL computer list)
 do
     $SAMBA_TOOL dns add localhost $SAMBA_CRED computer.$SAMBA_REALM   $name.$SAMBA_REALM A     $IPV4_ADDR
     $SAMBA_TOOL dns add localhost $SAMBA_CRED computer.$SAMBA_REALM *.$name.$SAMBA_REALM A     $IPV4_ADDR
-    $SAMBA_TOOL dns add localhost $SAMBA_CRED 10.10.10.in-addr.arpa 10 PTR   $name.$SAMBA_REALM
-    $SAMBA_TOOL dns add localhost $SAMBA_CRED 10.10.10.in-addr.arpa 10 PTR *.$name.$SAMBA_REALM
+    $SAMBA_TOOL dns add localhost $SAMBA_CRED $SAMBA_ZONE_PTR 10 PTR   $name.$SAMBA_REALM
+    $SAMBA_TOOL dns add localhost $SAMBA_CRED $SAMBA_ZONE_PTR 10 PTR *.$name.$SAMBA_REALM
     # add SRV record for each computer to srv zone
     $SAMBA_TOOL dns add localhost $SAMBA_CRED srv.$SAMBA_REALM srv.$name SRV '$name.$SAMBA_REALM 8080 0 100'
 done
